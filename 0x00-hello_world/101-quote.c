ada3e99a37fb702860ebb4e2f5b659eedded588c
Mabende <belindamabende@gmail.com>
2023-02-16 14:12:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program hat prints a line to a standard error
+ * main - A c  program that prints a line to the standard error
  * Return 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
-	retun(1);
+	
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }

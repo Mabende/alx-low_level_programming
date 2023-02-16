2c566a539b5dfe36600fe8ea9a82419b14207bf5
Mabende <belindamabende@gmail.com>
2023-02-16 13:52:56 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program hat prints a line to a standard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
+	retun(1);
+}

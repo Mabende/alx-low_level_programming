a6d6d4a951c39237a5912d4dec061b35edcf123c
Mabende <belindamabende@gmail.com>
2023-02-28 20:20:19 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,9 @@
-#`include "main.h"
+#include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer-prints all minutes in a day from 00:00
+ *
+ * Return:0 if succeesful.
  */
 void jack_bauer(void)
 {
@@ -26,6 +27,17 @@ void jack_bauer(void)
 		i++;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 

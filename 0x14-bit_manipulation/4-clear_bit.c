a9970a1860cfb560e054fdb6f81b68bcc6a9407f
Mabende <belindamabende@gmail.com>
2023-04-17 04:44:10 -0700

    updated

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,13 +3,13 @@
 /**
  * clear_bit - sets the value of a  bit to 0 at a given index
  * @n: pointer to the number to change
- * @index: index of the bit to clear
+ * @index: index of the bit set to 0
  *
- * Return: 1 for success, -1 if an error occured
+ * Return: 1 if successful, -1 if an error occured
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	*n = (~(1UL << index) & *n);

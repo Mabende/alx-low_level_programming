d597c4844879b162fe34db4d8dda3cc79b2894d1
Mabende <belindamabende@gmail.com>
2023-04-17 02:14:47 -0700

    updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,24 +11,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, bytes_written, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+		len = strlen(text_content);
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
 
-	close(o);
+	bytes_written = write(fd, text_content, len);
+	close(fd);
+
+	if (bytes_written == -1)
+		return (-1);
 
 	return (1);
 }

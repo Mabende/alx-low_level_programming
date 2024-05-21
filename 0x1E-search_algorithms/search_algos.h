02e03bb8b534dfb115f138132d104eb308511c02
Mabende <belindamabende@gmail.com>
2024-05-21 06:10:12 -0700

    Search Algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_value_checked(int value);
+void print_search_array(int *array, size_t start, size_t end);
+#endif
